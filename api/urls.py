a326301eb709a9424c48fa96c17bb1824d455284
Buzzi <kingbuzzman@gmail.com>
2011-11-12 16:59:34 -0500

    Renamed url in api

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, url
 from views import *
 
 urlpatterns = patterns('api.views',
-    url(r'^comment/?$', Comments.as_view(), name="comments"),
-    url(r'^comment/(?P<comment_id>\d+)/?$', Comment.as_view(), name="comment"),
-    url(r'^comment/(?P<comment_id>\d+)/rate/?$', CommentRating.as_view(), name="comment-ratings"),
+    url(r'^comments/?$', Comments.as_view(), name="comments"),
+    url(r'^comments/(?P<comment_id>\d+)/?$', Comment.as_view(), name="comment"),
+    url(r'^comments/(?P<comment_id>\d+)/rate/?$', CommentRating.as_view(), name="comment-ratings"),
 )
