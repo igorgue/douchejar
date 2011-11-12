449234f98eba4c423b35a8cba6fc1b3f087cf187
Igor Guerrero Fonseca <igfgt1@gmail.com>
2011-11-12 15:36:47 -0500

    blank=False and null=False are the defaults.

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Comment(models.Model):
-    comment = models.CharField(max_length=140, blank=False, null=False)
+    comment = models.CharField(max_length=140)
     price = models.DecimalField(decimal_places=2, max_digits=9, default=1.00)
     user = models.ForeignKey(User, db_index=True)
     created_date = models.DateTimeField(auto_now_add=True)
@@ -12,4 +12,4 @@ class Rating(models.Model):
     comment = models.ForeignKey(Comment, db_index=True)
 
 class Organization(models.Model):
-    name = models.CharField(max_length=255, blank=False, null=False)
+    name = models.CharField(max_length=255)
