10684789d34cd543c45e75e554209a3fccc90c12
Buzzi <kingbuzzman@gmail.com>
2011-11-12 15:05:50 -0500

    Added created_date to the comment model

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ class Comment(models.Model):
     comment = models.CharField(max_length=140, blank=False, null=False)
     price = models.DecimalField(decimal_places=2, max_digits=9, default=1.00)
     user = models.ForeignKey(User, db_index=True)
+    created_date = models.DateTimeField(auto_now_add=True)
 
 
 class Rating(models.Model):
