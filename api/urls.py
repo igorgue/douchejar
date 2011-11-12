5b4c42f27a9ab4d6167a30dcfe3e9ca5a5e21963
Buzzi <kingbuzzman@gmail.com>
2011-11-12 16:58:06 -0500

    Fix mistake with url

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,5 @@ from views import *
 urlpatterns = patterns('api.views',
     url(r'^comment/?$', Comments.as_view(), name="comments"),
     url(r'^comment/(?P<comment_id>\d+)/?$', Comment.as_view(), name="comment"),
-    url(r'^comment/(?P<comment_id>\d+)/rate/?$', Comment.as_view(), name="comment-ratings"),
+    url(r'^comment/(?P<comment_id>\d+)/rate/?$', CommentRating.as_view(), name="comment-ratings"),
 )
