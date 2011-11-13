f3083e20f8c0240fdc3e9fc5885e720d3a5c4b69
Buzzi <kingbuzzman@gmail.com>
2011-11-12 23:10:30 -0500

    Added logout, messing with permisions

--- app/urls.py
+++ app/urls.py
@@ -4,3 +4,7 @@ urlpatterns = patterns('app.views',
     url(r'^comments/(?P<comment_id>\d+)/?$', 'comment', name="comment"),
     url(r'^$', 'home', name="home"),
 )
+
+urlpatterns += patterns('',
+    url(r'^logout/?$', 'django.contrib.auth.views.logout', { 'next_page': '/' }, name='logout'),
+)
